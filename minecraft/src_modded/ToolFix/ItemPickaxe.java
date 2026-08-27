05ef657a0fd0a1c8ac948963135c381c79557129
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-08-26 22:17:41 -0700

    updated toolfix to include gold silk touch (not tested thoroughly)

--- minecraft/src_modded/ToolFix/ItemPickaxe.java
+++ minecraft/src_modded/ToolFix/ItemPickaxe.java
@@ -8,6 +8,7 @@ public class ItemPickaxe extends ItemTool {
 	}
 
 	public boolean canHarvestBlock(Block var1) {
-		return var1 == Block.obsidian ? this.toolMaterial.getHarvestLevel() == 3 : (var1 != Block.blockDiamond && var1 != Block.oreDiamond ? (var1 != Block.blockGold && var1 != Block.oreGold ? (var1 != Block.blockSteel && var1 != Block.oreIron ? (var1 != Block.blockLapis && var1 != Block.oreLapis ? (var1 != Block.oreRedstone && var1 != Block.oreRedstoneGlowing ? (var1.blockMaterial == Material.rock ? true : var1.blockMaterial == Material.iron) : this.toolMaterial.getHarvestLevel() >= 2) : this.toolMaterial.getHarvestLevel() >= 1) : this.toolMaterial.getHarvestLevel() >= 1) : this.toolMaterial.getHarvestLevel() >= 2) : this.toolMaterial.getHarvestLevel() >= 2);
+		int var2 = this.toolMaterial == EnumToolMaterial.GOLD && Block.goldSilkTouchConfig.getProperty("GoldSilkTouch").equals("1") ? 3 : this.toolMaterial.getHarvestLevel();
+		return var1 == Block.obsidian ? var2 == 3 : (var1 != Block.blockDiamond && var1 != Block.oreDiamond ? (var1 != Block.blockGold && var1 != Block.oreGold ? (var1 != Block.blockSteel && var1 != Block.oreIron ? (var1 != Block.blockLapis && var1 != Block.oreLapis ? (var1 != Block.oreRedstone && var1 != Block.oreRedstoneGlowing ? (var1.blockMaterial == Material.rock ? true : var1.blockMaterial == Material.iron) : var2 >= 2) : var2 >= 1) : var2 >= 1) : var2 >= 2) : var2 >= 2);
 	}
 }
---
+++ minecraft/src_modded/ToolFix/ItemTallGrass.java
@@ -0,0 +1,24 @@
+package net.minecraft.src;
+
+public class ItemTallGrass extends ItemBlock {
+	public ItemTallGrass(int var1) {
+		super(var1);
+		this.setHasSubtypes(true);
+	}
+
+	public int getPlacedBlockMetadata(int var1) {
+		return var1;
+	}
+
+	public int getIconFromDamage(int var1) {
+		return Block.tallGrass.getBlockTextureFromSideAndMetadata(0, var1);
+	}
+
+	public int getColorFromDamage(int var1) {
+		return ColorizerGrass.getGrassColor(0.5D, 1.0D);
+	}
+
+	public String getItemNameIS(ItemStack var1) {
+		return var1.getItemDamage() == 2 ? super.getItemName() + ".fern" : super.getItemName() + ".grass";
+	}
+}
