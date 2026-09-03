0a37541f873cdf8f39e9c26b4f7f7ee0f6675639
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-09-02 21:51:57 -0700

    added scuffed build script

---
+++ reobf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if ! git diff --quiet; then
+    echo "Error: Unstaged changes detected, commit or stash them before building."
+    exit 1
+fi
+
+rm -rf minecraft/reobf minecraft/src
+java -jar ../../RetroMCP-Java/build/libs/RetroMCP-CLI-all.jar decompile
+git restore .
+java -jar ../../RetroMCP-Java/build/libs/RetroMCP-CLI-all.jar recompile
+java -jar ../../RetroMCP-Java/build/libs/RetroMCP-CLI-all.jar reobfuscate
+cp -r minecraft/resources/* minecraft/reobf
+rm -f Parker_mod.zip
+cd minecraft/reobf/
+zip -r ../../Parker_mod.zip .
+cd ../../
